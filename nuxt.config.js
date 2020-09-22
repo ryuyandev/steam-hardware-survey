47224cce0dfdd8f5147405fbe33316b027e8504c
Ryan <ryuyan@pm.me>
2020-09-22 01:17:02 -0400

    Changed cron to daily

--- nuxt.config.js
+++ nuxt.config.js
@@ -104,7 +104,7 @@ export default {
   hooks: {
     listen(server, listener) {
       const dataGeneration = new CronJob({
-        cronTime: '00 00 00 1-2 * *',
+        cronTime: '00 00 00 * * *',
         runOnInit: true,
         start: true,
         onTick() {
